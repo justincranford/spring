ec213325596420e772c1b8ccb10fe54c3705fcea
Justin Cranford <justincranford@hotmail.com>
2023-03-14 23:12:28 -0400

    Consolidate two ControllerAdvice classes into a single class.

--- src/main/java/com/github/justincranford/spring/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/github/justincranford/spring/controller/ExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.github.justincranford.spring.controller;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.github.justincranford.spring.model.AppUserNotFoundException;
 import com.github.justincranford.spring.model.OpsUserNotFoundException;
 
 @ControllerAdvice
-public class ApiResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler({ OpsUserNotFoundException.class })
+	@ExceptionHandler({ OpsUserNotFoundException.class, AppUserNotFoundException.class })
 	protected ResponseEntity<Object> handleNotFound(final Exception ex, final WebRequest request) {
 		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+
+	@ExceptionHandler({ ConstraintViolationException.class, DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleBadRequest(final Exception ex, final WebRequest request) {
+		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 }

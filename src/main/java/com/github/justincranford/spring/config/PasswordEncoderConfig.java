e324e3ac53180404a79a5b1dd8291b78db33040c
Justin Cranford <justincranford@hotmail.com>
2023-03-16 23:58:49 -0400

    Cleanup comments

--- src/main/java/com/github/justincranford/spring/config/PasswordEncoderConfig.java
+++ src/main/java/com/github/justincranford/spring/config/PasswordEncoderConfig.java
@@ -12,23 +12,22 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @Configuration
-@EnableWebSecurity
 public class PasswordEncoderConfig {
     @Value(value="${spring.application.name}") protected String springApplicationName;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-//		final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//		final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		final String defaultEncoderId = "pbkdf2v1";
-		final PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(defaultEncoderId,
+		// See: PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		final PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(
+			// TODO: Change from pbkdf2v0 (alpha) to pbkdf2v1 (production)
+			// algorithm to use for hashing new passwords (only one can be defined)
+			"pbkdf2v0",
+			// algorithms to use for decoding parameters of stored password hashes, for comparing challenge passwords
 			Map.of(
-//				"null", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance(),
+				"null", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance(),
 				"noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance(),
-				// TODO Remove v1 with only 1 iteration and SHA1
-				"pbkdf2v1", new Pbkdf2PasswordEncoder(this.springApplicationName, 16, 1, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA1)
-//				"pbkdf2v1", new Pbkdf2PasswordEncoder(this.springApplicationName, 16, 310_000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
-//				"pbkdf2v2", new Pbkdf2PasswordEncoder(this.springApplicationName, 16, 120_000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA512)
+				"pbkdf2v0", new Pbkdf2PasswordEncoder(this.springApplicationName, 16, 1,       SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA1),
+				"pbkdf2v1", new Pbkdf2PasswordEncoder(this.springApplicationName, 16, 310_000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256),
 			));
 		return passwordEncoder;
 	}

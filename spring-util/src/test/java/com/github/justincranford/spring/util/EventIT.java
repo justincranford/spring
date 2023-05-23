d97016e566e9bcd6132a5d9329741393c3aa7c98
Justin Cranford <justincranford@hotmail.com>
2023-05-23 01:12:07 -0400

    Add event publish/listen test.

---
+++ spring-util/src/test/java/com/github/justincranford/spring/util/EventIT.java
@@ -0,0 +1,30 @@
+package com.github.justincranford.spring.util;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
+
+import com.github.justincranford.spring.util.config.EventsConfig;
+
+public class EventIT extends AbstractIT {
+	@Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+	@SpyBean
+	@Qualifier("allApplicationEventsListener")
+	public ApplicationListener<ApplicationEvent> allApplicationEventsListener;
+
+	@Test
+	public void testPublishListenEvent() throws Exception {
+        final EventsConfig.Event<String> event = new EventsConfig.Event<>("testPublishListenEvent");
+		this.applicationEventPublisher.publishEvent(event);
+		verify(this.allApplicationEventsListener, times(1)).onApplicationEvent(event);
+    }
+}

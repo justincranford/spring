dbc37805e1d9c8ddf8987fc87cc654b83883ed01
Justin Cranford <justincranford@hotmail.com>
2023-06-15 01:30:46 -0400

    Remove unused import

--- spring-authn-server/src/test/java/com/github/justincranford/spring/authn/server/AbstractIT.java
+++ spring-authn-server/src/test/java/com/github/justincranford/spring/authn/server/AbstractIT.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;

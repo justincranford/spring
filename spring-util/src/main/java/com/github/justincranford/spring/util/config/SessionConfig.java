4b9da311747e0f5f7c7e07c93804ac3f4493ea0e
Justin Cranford <justincranford@hotmail.com>
2023-06-19 21:38:47 -0400

    Remove unused logger.

--- spring-util/src/main/java/com/github/justincranford/spring/util/config/SessionConfig.java
+++ spring-util/src/main/java/com/github/justincranford/spring/util/config/SessionConfig.java
@@ -1,7 +1,5 @@
 package com.github.justincranford.spring.util.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,16 +10,11 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @Configuration
 @EnableWebSecurity
 public class SessionConfig {
-    @SuppressWarnings("unused")
-	private Logger logger = LoggerFactory.getLogger(SessionConfig.class);
-
-    // spring security core
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
     }
 
-    // spring security core
     @Bean
     public HttpSessionEventPublisher httpSessionEventPublisher() {
         return new HttpSessionEventPublisher();
